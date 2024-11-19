fd9f31e3e301ea8feda8fd80c5aa623f968dcb10
LuisMatheusVdeFarias <116664337+luismatheus2006@users.noreply.github.com>
2024-11-19 20:04:25 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main()
+{
+    int linhas , i , colunas, A;
+    scanf("%d", &linhas);
+    scanf("%d", &colunas);
+    
+    int **vetor;
+    vetor = (int**)malloc(linhas * sizeof(int*));
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
-
-    return 0;
+    for(i = 0; i < linhas; i++){
+        vetor[i] = (int*)malloc(colunas * sizeof(int));
+    }
+    
+    for(i = 0; i < linhas; i++){
+    	
+       for(A = 0; A < colunas; A++){
+       		scanf("%d", &vetor[i][A]);
+       		
+    	}
+    }
+    
+    for(i = 0; i < linhas; i++){
+       for(A = 0; A < colunas; A++){
+       		printf("%d",vetor[i][A]);
+    	}
+    printf("\n");
+    }
+    
+    for(i = 0; i < linhas; i++){
+       free(vetor[i]);
+    }
+    free(vetor);
 }
