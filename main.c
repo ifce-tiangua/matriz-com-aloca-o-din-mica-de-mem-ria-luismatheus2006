f54e5dd3656e0bf0caab44bd9f0f36e4e2598731
LuisMatheusVdeFarias <116664337+luismatheus2006@users.noreply.github.com>
2024-11-21 16:00:40 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,39 +3,43 @@
 
 int main()
 {
-    
-    int linhas , i , colunas, A;
+    int linhas, i, colunas, A;
     scanf("%d", &linhas);
     scanf("%d", &colunas);
-    if(linhas == 0 || linhas < 0 || colunas == 0 || colunas < 0){
-        printf("[matriz vazia]");
+
+    if (linhas <= 0 || colunas <= 0) {
+        printf("[matriz vazia]\n");
         exit(0);
     }
-    
+
     int **vetor;
     vetor = (int**)malloc(linhas * sizeof(int*));
 
-    for(i = 0; i < linhas; i++){
+    for (i = 0; i < linhas; i++) {
         vetor[i] = (int*)malloc(colunas * sizeof(int));
     }
-    
-    for(i = 0; i < linhas; i++){
-    	
-       for(A = 0; A < colunas; A++){
-       		scanf("%d", &vetor[i][A]);
-       		
-    	}
+
+    for (i = 0; i < linhas; i++) {
+        for (A = 0; A < colunas; A++) {
+            scanf("%d", &vetor[i][A]);
+        }
     }
-    
-    for(i = 0; i < linhas; i++){
-       for(A = 0; A < colunas; A++){
-       		printf("%d",vetor[i][A]);
-    	}
-    printf("\n");
+
+    for (i = 0; i < linhas; i++) {
+        for (A = 0; A < colunas; A++) {
+            // Verifica se não é o último elemento da linha para evitar o espaço extra
+            if (A == colunas - 1) {
+                printf("%d", vetor[i][A]);
+            } else {
+                printf("%d ", vetor[i][A]);
+            }
+        }
+        printf("\n");
     }
-    
-    for(i = 0; i < linhas; i++){
-       free(vetor[i]);
+
+    for (i = 0; i < linhas; i++) {
+        free(vetor[i]);
     }
     free(vetor);
+
 }
